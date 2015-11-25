5782ad93d436468c780c7e371e1ac916fc37af87
Matt Comben <matt.comben@itdev.co.uk>
2015-11-25 12:13:48 +0000

    Moved configuration items into config.py and updated to allow parameters to be passed through environmental variables

--- tuckshop/app/config.py
+++ tuckshop/app/config.py
@@ -1,2 +1,7 @@
-LDAP_SERVER = 'localhost'
-SHOW_OUT_OF_STOCK_ITEMS = True
+from os import environ
+
+LDAP_SERVER = 'localhost' if 'LDAP_SERVER' not in environ else environ['LDAP_SERVER']
+SHOW_OUT_OF_STOCK_ITEMS = True if 'SHOW_OUT_OF_STOCK_ITEMS' not in environ else environ['SHOW_OUT_OF_STOCK_ITEMS']
+TRANSACTION_PAGE_SIZE = 10 if 'TRANSACTION_PAGE_SIZE' not in environ else environ['TRANSACTION_PAGE_SIZE']
+TOTAL_PAGE_DISPLAY = 7 if 'TOTAL_PAGE_DISPLAY' not in environ else environ['TOTAL_PAGE_DISPLAY']
+APP_NAME = 'ITDev Tuck Shop' if 'APP_NAME' not in environ else environ['APP_NAME']
