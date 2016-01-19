6bce16ffe4bc7f0e6910119da5c0a938d3a6d334
Matthew Comben <matt.comben@itdev.co.uk>
2016-01-19 23:08:40 +0000

    Changed line indentation from 2 to 4 spaces in redis_connection.py
    Refs T52

--- tuckshop/core/redis_connection.py
+++ tuckshop/core/redis_connection.py
@@ -6,77 +6,77 @@ import redis
 
 class LocalRedis(object):
 
-  def __init__(self):
-    self.cache = {}
+    def __init__(self):
+        self.cache = {}
 
-  def hset(self, name, key, value):
-    if (name not in self.cache):
-      self.cache[name] = {}
-    self.cache[name][key] = value
-    return True
+    def hset(self, name, key, value):
+        if (name not in self.cache):
+            self.cache[name] = {}
+        self.cache[name][key] = value
+        return True
 
-  def hget(self, name, key):
-    if (name in self.cache and key in self.cache[name]):
-      return self.cache[name][key]
-    else:
-      return None
+    def hget(self, name, key):
+        if (name in self.cache and key in self.cache[name]):
+            return self.cache[name][key]
+        else:
+            return None
 
-  def set(self, key, value):
-    self.cache[key] = value
-    return True
+    def set(self, key, value):
+        self.cache[key] = value
+        return True
 
-  def get(self, key):
-    if key in self.cache:
-      return self.cache[key]
-    else:
-      return None
+    def get(self, key):
+        if key in self.cache:
+            return self.cache[key]
+        else:
+            return None
 
-  def delete(self, names):
-    for name in names:
-      if name in self.cache:
-        del(self.cache[name])
+    def delete(self, names):
+        for name in names:
+            if name in self.cache:
+                del(self.cache[name])
 
-    return True
+        return True
 
-  def exists(self, name):
-    return (name in self.cache)
+    def exists(self, name):
+        return (name in self.cache)
 
 
 class RedisConnection(object):
 
-  CONNECTION = None
+    CONNECTION = None
 
-  @staticmethod
-  def _getConnection():
-    if not RedisConnection.CONNECTION:
-      if 'REDIS_URL' in os.environ:
-        url = urlparse.urlparse(url=os.environ['REDIS_URL'])
-        RedisConnection.CONNECTION = redis.Redis(host=url.hostname, port=url.port)
-      else:
-        RedisConnection.CONNECTION = LocalRedis()
+    @staticmethod
+    def _getConnection():
+        if not RedisConnection.CONNECTION:
+            if 'REDIS_URL' in os.environ:
+                url = urlparse.urlparse(url=os.environ['REDIS_URL'])
+                RedisConnection.CONNECTION = redis.Redis(host=url.hostname, port=url.port)
+            else:
+                RedisConnection.CONNECTION = LocalRedis()
 
-    return RedisConnection.CONNECTION
+        return RedisConnection.CONNECTION
 
-  @staticmethod
-  def hset(name, key, value):
-    return RedisConnection._getConnection().hset(name, key, value)
+    @staticmethod
+    def hset(name, key, value):
+        return RedisConnection._getConnection().hset(name, key, value)
 
-  @staticmethod
-  def hget(name, key):
-    return RedisConnection._getConnection().hget(name, key)
+    @staticmethod
+    def hget(name, key):
+        return RedisConnection._getConnection().hget(name, key)
 
-  @staticmethod
-  def set(key, value):
-    return RedisConnection._getConnection().set(key, value)
+    @staticmethod
+    def set(key, value):
+        return RedisConnection._getConnection().set(key, value)
 
-  @staticmethod
-  def get(key):
-    return RedisConnection._getConnection().get(key)
+    @staticmethod
+    def get(key):
+        return RedisConnection._getConnection().get(key)
 
-  @staticmethod
-  def delete(names):
-    return RedisConnection._getConnection().delete(names)
+    @staticmethod
+    def delete(names):
+        return RedisConnection._getConnection().delete(names)
 
-  @staticmethod
-  def exists(name):
-    return RedisConnection._getConnection().exists(name)
+    @staticmethod
+    def exists(name):
+        return RedisConnection._getConnection().exists(name)
