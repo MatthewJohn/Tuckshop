e51bd3df5356544b830e2a7b21d87d801d5ce3bc
Matt Comben <matt.comben@itdev.co.uk>
2016-01-21 08:54:36 +0000

    Added handling of exceptions and implemented TuckshopExceptions, where error is shown to user
    Fixes T20

--- tuckshop/page/admin.py
+++ tuckshop/page/admin.py
@@ -3,6 +3,7 @@
 from tuckshop.app.models import User
 from tuckshop.page.page_base import PageBase
 from tuckshop.core.utils import getMoneyString
+from tuckshop.core.tuckshop_exception import TuckshopException
 
 class Admin(PageBase):
 
@@ -22,13 +23,12 @@ class Admin(PageBase):
         action = self.post_vars['action'] if 'action' in self.post_vars else None
         if action == 'pay_stock':
             if 'amount' not in self.post_vars:
-                self.return_vars['error'] = 'Amount to pay must be specified and be a positive amount'
-                return
+                raise TuckshopException('Amount to pay must be specified and be a positive amount')
             try:
                 float(self.post_vars['amount'])
             except:
-                self.return_vars['error'] = 'Amount to pay must be specified and be a positive amount'
-                return
+                raise TuckshopException('Amount to pay must be specified and be a positive amount')
+
             amount = int(float(self.post_vars['amount']) * 100)
             user = User.objects.get(uid=self.post_vars['uid'])
             amount, semi_paid_transaction = user.payForStock(amount)
@@ -41,8 +41,8 @@ class Admin(PageBase):
 
         elif action == 'credit':
             if 'amount' not in self.post_vars or not int(self.post_vars['amount']):
-                self.return_vars['error'] = 'Amount must be a positive integer'
-                return
+                raise TuckshopException('Amount must be a positive integer')
+
             description = None if 'description' not in self.post_vars else self.post_vars['description']
             user = User.objects.get(uid=self.post_vars['uid'])
             user.addCredit(int(self.post_vars['amount']), description=description)
@@ -50,8 +50,8 @@ class Admin(PageBase):
                                                            user.uid)
         elif action == 'debit':
             if 'amount' not in self.post_vars or not int(self.post_vars['amount']):
-                self.return_vars['error'] = 'Amount must be a positive integer'
-                return
+                raise TuckshopException('Amount must be a positive integer')
+
             description = None if 'description' not in self.post_vars else self.post_vars['description']
             user = User.objects.get(uid=self.post_vars['uid'])
             user.removeCredit(int(self.post_vars['amount']), description=description)
