0199257dce2d7edf48226170d5b8860c4adc412d
Matt Comben <matt.comben@itdev.co.uk>
2016-01-27 08:35:59 +0000

    Added confirmation dialog whilst performing custom payments
    Refs T93

--- js/credit-dialog.js
+++ js/credit-dialog.js
@@ -22,3 +22,41 @@ function purchaseItem(event)
     });
     return false;
 }
+
+function customPurchase(event)
+{
+    var description = event.target.getElementsByClassName('custom-amount-description')[0].value || "";
+    var item_value = event.target.getElementsByClassName('custom-amount-input')[0].value;
+    description_message = '';
+    description_warning = '';
+    if (description != '')
+    {
+        description_message = ', using description <code>' + description + '</code>';
+    }
+    else
+    {
+        description_warning = '<br /><b>You have not provided a description. It is highly recommended to use one!</b><br />';
+    }
+    message = 'Are you sure you wish to perform a custom payment of ' + item_value + 'p' + description_message + '?' + description_warning +
+              '<br /><br /><b>Note:</b> It is generally best to purchase items, rather than performing a custom payment<br >' +
+              '<b>Only perform a custom payment if it is absolutely necessary!</b>'
+
+    BootstrapDialog.show({
+        message: message,
+        buttons: [{
+            label: 'Purchase',
+            action: function(dialogRef) {
+                event.target.submit();
+                dialogRef.close()
+            },
+            cssClass: 'btn-warning'
+        },
+        {
+            label: 'Cancel',
+            action: function(dialogRef) {
+                dialogRef.close();
+            }
+        }]
+    });
+    return false;
+}
