5ac181f206586950a0760c69cf405f5afbbc151b
Matt Comben <matthew@dockstudios.co.uk>
2017-01-31 06:33:27 +0000

    Correct app in permission migration

--- tuckshop/app/migrations/0030_set_allowed_permissions.py
+++ tuckshop/app/migrations/0030_set_allowed_permissions.py
@@ -7,13 +7,13 @@ from tuckshop.core.permission import Permission
 def set_default_permissions(apps, schema_editor):
     User = apps.get_model('app', 'User')
     for row in User.objects.all():
-        row.add_permission(Permission.ACCESS_CREDIT_PAGE)
-        row.add_permission(Permission.ACCESS_STOCK_PAGE)
+        row.addPermission(Permission.ACCESS_CREDIT_PAGE)
+        row.addPermission(Permission.ACCESS_STOCK_PAGE)
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myapp', '0029_user_skype_id'),
+        ('app', '0029_user_skype_id'),
     ]
 
     operations = [
